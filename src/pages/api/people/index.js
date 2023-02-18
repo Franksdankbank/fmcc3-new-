7e01fbe104e96b11144cafed038fa9ce15aa3a62
Franksdankbank <89324801+Franksdankbank@users.noreply.github.com>
2023-02-18 16:21:03 -0500

    guy on youtube video
    basically the code in the index.js files and people.js lifes in the mongo folder in the lib folder allow me to cache my connection are reuseit.

---
+++ src/pages/api/people/index.js
@@ -0,0 +1,19 @@
+import { getPeople } from '@lib/mongo/people'
+
+const handler = async (req, res) => {
+    if(req.method === 'GET'){
+        try {
+            const { people, error } = await getPeople()
+            if (error) throw new Error(error)
+
+            return res.status(200).json({ people })
+        } catch (error) {
+            return res.status(500).json({ error: error.message })
+        }
+    }
+    res.setHeader('Allow', ['GET'])
+    res.status(425).end(`Method ${req.method} is not allowed.`)
+}
+
+export default handler
+
